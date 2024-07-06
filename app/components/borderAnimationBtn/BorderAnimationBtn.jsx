5c86f1e8715f9c0b6bfa16ad2e3297b27a73d763
Thejellyfish1024 <alamin102410@gmail.com>
2024-07-06 16:18:50 +0600

    modified the animation code

--- app/components/borderAnimationBtn/BorderAnimationBtn.jsx
+++ app/components/borderAnimationBtn/BorderAnimationBtn.jsx
@@ -3,12 +3,12 @@ import './style.css';
 const BorderAnimationBtn = () => {
     return (
         <button
-            className="button  text-sm text-gray-400 px-4 py-2">
+            className="button ">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
-            Exclusive Webinar: Ai Agents for Customer Support <strong className='font-bold text-white ml-2'>Sign Up</strong>
+            <p className='bg-black children  text-sm text-gray-400 px-4 py-2'>Exclusive Webinar: Ai Agents for Customer Support <strong className='font-bold text-white ml-2'>Sign Up</strong></p>
         </button>
     );
 };
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import BorderAnimationBtn from "./components/borderAnimationBtn/BorderAnimationB
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
-      <BorderAnimationBtn/>
+      <BorderAnimationBtn />
     </main>
   );
 }
