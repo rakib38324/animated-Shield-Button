a2a60bba1183050dd8f1e39c83fde799e598713a
Thejellyfish1024 <alamin102410@gmail.com>
2024-07-06 13:13:53 +0600

    first commit

---
+++ app/components/borderAnimationBtn/BorderAnimationBtn.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+
+const BorderAnimationBtn = () => {
+    return (
+        <button
+            className="button  text-sm text-gray-400 px-4 py-2">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Exclusive Webinar: Ai Agents for Customer Support <strong className='font-bold text-white ml-2'>Sign Up</strong>
+        </button>
+    );
+};
+
+export default BorderAnimationBtn;
---
+++ app/components/imageAnimation/ImageAnimation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageAnimation = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ImageAnimation;
